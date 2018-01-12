529212894649450000657bb31171ef4ba138a0cf
Joris Jonkers Both <joris@jonkersboth.eu>
2018-01-12 11:20:27 +0100

    Reduced code size forest-filter

--- forest-filter.py
+++ forest-filter.py
@@ -1,11 +1,7 @@
 import numpy as np
 
 with open('joined_db2.csv') as f:
-    forestEntries = []
-    for line in f:
-        if 'Forest' in line:
-            forestEntries.append(line)
-
     with open('forest_filter_result.csv', 'a') as result_file:
-        for entry in forestEntries:
-            result_file.write(entry)
+        for line in f:
+            if 'Forest' in line:
+                result_file.write(line)
