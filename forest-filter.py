1b3f5044ef11ee7efaa17686a46dbbdcc189873e
Joris Jonkers Both <joris@jonkersboth.eu>
2018-01-12 11:08:16 +0100

    Added forest-filter script

---
+++ forest-filter.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+with open('joined_db2.csv') as f:
+    forestEntries = []
+    for line in f:
+        if 'Forest' in line:
+            forestEntries.append(line)
+
+    with open('forest_filter_result.csv', 'a') as result_file:
+        for entry in forestEntries:
+            result_file.write(entry)
