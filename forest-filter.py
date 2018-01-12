1d02acc5aab622508aee39d749f15788aae90552
Patrick Spaans <pspaans@hotmail.nl>
2018-01-12 13:46:24 +0100

    Added all latin names of tree species and updated the forest-filter accordingly

--- forest-filter.py
+++ forest-filter.py
@@ -1,7 +1,12 @@
-#  Filter the Forest
+treespecies = []
+with open('treespecies') as b:
+    for species in b:
+        treespecies.append(species)
 
 with open('joined_db2.csv') as f:
     with open('forest_filter_result.csv', 'a') as result_file:
         for line in f:
             if 'Forest' in line:
-                result_file.write(line)
+                for species in treespecies:
+                    if species in line:
+                        result_file.write(line)
