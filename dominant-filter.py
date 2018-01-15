ee3b077d85493a466493bb03f0ef0c2a719f662b
Patrick Spaans <pspaans@hotmail.nl>
2018-01-15 10:17:46 +0100

    fixed the dominant filter so that it no longer lists random trees instead of the dominant ones

--- dominant-filter.py
+++ dominant-filter.py
@@ -1,28 +1,30 @@
 import csv
 import operator
 
-dominant_tree_ids = []
 with open('forest_filter_result.csv') as f:
+    with open('dominant_trees.csv', 'a') as result_file:
         convertedf = csv.reader(f, delimiter=';')
         sortedf = sorted(convertedf, key=operator.itemgetter(5), reverse=True)
         current_polygon = ''
         dominant_tree = ''
         dominant_tree_ratio = 0
-        linecount = 0
         for line in sortedf:
-            if line[5] != current_polygon:
-                current_polygon = line[5]
+            if line[1] != current_polygon:
+                if current_polygon != '':
+                    for data in dominant_tree:
+                        rebuildline = rebuildline + data
+                        if data != dominant_tree[-1]:
+                            rebuildline = rebuildline + ';'
+                    result_file.write(rebuildline + '\n')
+                current_polygon = line[1]
                 dominant_tree_ratio = 0
-                dominant_tree_ids.append(linecount)
+                rebuildline = ''
             if float(line[3]) > float(dominant_tree_ratio):
-                dominant_tree = linecount
+                dominant_tree = line
                 dominant_tree_ratio = line[3]
-            linecount += 1
+        for data in dominant_tree:
+            rebuildline = rebuildline + data
+            if data != dominant_tree[-1]:
+                rebuildline = rebuildline + ';'
+        result_file.write(rebuildline + '\n')
 
-with open('forest_filter_result.csv') as f:
-    with open('dominant_trees.csv', 'a') as result_file:
-        linecount = 0
-        for line in f:
-            if linecount in dominant_tree_ids:
-                result_file.write(line)
-            linecount += 1
