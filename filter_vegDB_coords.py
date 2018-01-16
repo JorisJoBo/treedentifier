d9f56ce9f252664ad35a97fd4be25accb3aa9792
Thom Visser <thom.1@kpnmail.nl>
2018-01-16 10:40:48 +0100

    Bugfix filter_vegDB_coords.py

--- filter_vegDB_coords.py
+++ filter_vegDB_coords.py
@@ -29,7 +29,7 @@ plots = []
 for line in polys:
     X = []
     Y = []
-    polyID = line[1]
+    polyID = line[0]
     allCoords = line[1][9:-2]
     coords = allCoords.split(',')
     for coord in coords:
