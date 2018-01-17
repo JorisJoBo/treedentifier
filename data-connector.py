6c5d04ae081972e38b66e743c6ab766beaa28b40
Patrick Spaans <pspaans@hotmail.nl>
2018-01-17 14:04:17 +0100

    Data connector added, fuses canopy data with the latin names of the dominant tree dataset

--- data-connector.py
+++ data-connector.py
@@ -7,39 +7,29 @@ the labels.
 Returns a .csv file containing the data required for machinelearning.
 """
 
-import numpy as np
 import sys
+import csv
 
 # filename1 = "combined_canopy.csv"
 filename1 = sys.argv[1]
 # filename2 = "dominant_trees.csv"
 filename2 = sys.argv[2]
 
-raw_metrics = np.loadtxt(open(filename1, 'rb'), dtype=str, delimiter=',')
-trees = np.loadtxt(open(filename2, 'rb'), dtype=str, delimiter=';')
-
-# sort metrics to match trees
-sorted_metrics = []
-for line in trees:
-    polyID = line[1]
-    for row in raw_metrics:
-        if row[1] == polyID:
-            sorted_metrics.append(row)
-metrics = np.array(sorted_metrics)
-X = metrics[1:]
-
-# remove and sort trees to match sorted metrics
-sorted_trees = []
-for line in trees:
-    if line[1] in metrics[0, :]:
-        sorted_trees.append(line[:, -1])
-Y = np.array(sorted_trees)
-
-# write all relevant data to file
-X = X.toList()
-Y = Y.toList()
-with open('learning_data.csv', 'a') as result_file:
-    for line in range(len(X)):
-        stringLineX = ','.join(X[line])
-        stringLineY = ','.join(Y[line])
-        result_file.write(stringLineX + ',' + stringLineY + '\n')
+with open(filename1) as f1:
+    with open(filename2) as f2:
+        with open('learning_data.csv', 'a') as result_file:
+            f1 = csv.reader(f1, delimiter=',')
+            f2 = csv.reader(f2, delimiter=';')
+            latindict = {}
+            for line in f2:
+                latindict[line[1]] = line[7]
+            next(f1)
+            for line in f1:
+                latinname = latindict[line[1]]
+                line.append(latinname)
+                outputline = ''
+                for item in line:
+                    outputline = outputline + item
+                    if item != line[-1]:
+                        outputline = outputline + ';'
+                result_file.write(outputline + '\n')
