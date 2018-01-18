9d219dbf6fabaee8c4ad40b0d9787ca3d40c8730
Geerten Rijsdijk <geertenrijsdijk@hotmail.com>
2018-01-18 09:46:43 +0100

    add_canopy_files can now be used on both a directory and a single csv file

--- add_canopy_files.py
+++ add_canopy_files.py
@@ -1,10 +1,12 @@
 '''
  add_canopy_files.py
 
- usage: add_canopy_files.py input_directory output.csv
+ usage for map: add_canopy_files.py input_directory output.csv
+ usage for single file: add_canopy_files.py input.csv output.csv
 
  Adds together all csv files generated from lascanopy and removes lines
- with missing values.
+ with missing values. Can also be used to remove lines with missing values
+ from a single csv file.
  The map containing the csv files that have to be combined has to be
  present in the same directory as this file.
 '''
@@ -20,14 +22,22 @@ except BaseException:
 
 lines = []
 header = None
-for file in os.listdir(dirname):
-    if file.endswith(".csv"):
-        with open(dirname + '/' + file, 'r') as f:
-            for i, line in enumerate(f, 0):
-                if i != 0:
-                    lines.append(line)
-                else:
-                    header = line
+if dirname.endswith('.csv'):
+    with open(dirname, 'r') as f:
+        for i, line in enumerate(f, 0):
+            if i != 0:
+                lines.append(line)
+            else:
+                header = line
+else:
+    for file in os.listdir(dirname):
+        if file.endswith(".csv"):
+            with open(dirname + '/' + file, 'r') as f:
+                for i, line in enumerate(f, 0):
+                    if i != 0:
+                        lines.append(line)
+                    else:
+                        header = line
 
 with open(outputfile, 'w') as f:
     f.write(header)
