0213116444b085739aab97272301b00101cb57bd
Patrick Spaans <pspaans@hotmail.nl>
2018-01-23 11:17:05 +0100

    PATRICK WINS THE 1v1

--- outlier-filter.py
+++ outlier-filter.py
@@ -11,30 +11,28 @@ with open(filename) as f:
             if line[0] == 'index':
                 outputline = ''
                 for item in line:
-                    outputline = outputline + item
-                    if item != line[-1]:
-                        outputline = outputline + ';'
-                result_file.write(outputline + '\n')
+                    outputline = ''
+                    for item in line:
+                        outputline = outputline + item + ';'
+                    result_file.write(outputline[:-1] + '\n')
             else:
                 if line[1] != polygonID:
                     polygonID = line[1]
                     originalline = line
                     outputline = ''
                     for item in line:
-                        outputline = outputline + item
-                        if item != line[-1]:
-                            outputline = outputline + ';'
-                    result_file.write(outputline + '\n')
+                        outputline = ''
+                        for item in line:
+                            outputline = outputline + item + ';'
+                        result_file.write(outputline[:-1] + '\n')
                 else:
                     write = True
                     for i in range(6, len(line)):
                         difference = float(line[i]) - float(originalline[i])
-                        if difference**2 > (float(originalline[i]) * 0.5)**2:
+                        if difference**2 > (float(originalline[i]))**2:
                             write = False
                     if write == True:
                         outputline = ''
                         for item in line:
-                            outputline = outputline + item
-                            if item != line[-1]:
-                                outputline = outputline + ';'
-                        result_file.write(outputline + '\n')
+                            outputline = outputline + item + ';'
+                        result_file.write(outputline[:-1] + '\n')
