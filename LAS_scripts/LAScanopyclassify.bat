9f7909246104843a1abffcf70c0af30902b82201
Geerten Rijsdijk <geertenrijsdijk@hotmail.com>
2018-01-24 09:35:09 +0100

    Adjusted so that you can change the input_output map

--- LAS_scripts/LAScanopyclassify.bat
+++ LAS_scripts/LAScanopyclassify.bat
@@ -1,9 +1,9 @@
 @echo off
-set outputfolder=LiDAR_data
+set outputfolder=LiDAR_data2
 
 echo Running LAScanopy... (this may take a while)
 mkdir %outputfolder%\canopy
 
-lascanopy -i LiDAR_data\classify\*.laz -names -lor %outputfolder%\ID_growing_forest.csv -p 5 10 25 50 75 90 -min -max -avg -std -ske -kur -qav -c 10 50 -step 1 -o LiDAR_data\canopy\%%~na.csv
+lascanopy -i %outputfolder%\classify\*.laz -names -lor %outputfolder%\ID_growing_forest.csv -p 5 10 25 50 75 90 -min -max -avg -std -ske -kur -qav -c 10 50 -step 1 -o %outputfolder%\canopy\canopy_result.csv
 
 PAUSE
