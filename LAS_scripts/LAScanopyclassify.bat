68d49110eb1e96221788ac0d6f8cb891b78bcd02
Joris Jonkers Both <joris@jonkersboth.eu>
2018-01-24 09:20:21 +0100

    Removed '0' data from LAScanopyclassify.bat

--- LAS_scripts/LAScanopyclassify.bat
+++ LAS_scripts/LAScanopyclassify.bat
@@ -3,9 +3,7 @@ set outputfolder=LiDAR_data
 
 echo Running LAScanopy... (this may take a while)
 mkdir %outputfolder%\canopy
-for %%a in (%outputfolder%\classify\*.laz) do ( 
-	echo  - Running canopy on %%~nxa...
-	lascanopy -i LiDAR_data\classify\%%~nxa -names -lor LiDAR_data\ID_forest_grid_coords.csv -dns -p 5 10 25 50 75 90 -min -max -avg -std -ske -kur -qav -cov -c 2 4 10 50 -int_min -int_max -int_avg -int_qav -int_std -int_ske -int_kur -int_c 128 256 1024 -int_p 25 50 75 -o LiDAR_data\canopy\%%~na.csv
-)
+
+lascanopy -i LiDAR_data\classify\*.laz -names -lor %outputfolder%\ID_growing_forest.csv -p 5 10 25 50 75 90 -min -max -avg -std -ske -kur -qav -c 10 50 -step 1 -o LiDAR_data\canopy\%%~na.csv
 
 PAUSE
